ec4276f129705bb2f47b3d43c15c9ca4b5facdda
mathster101 <mathewsimonpanicker@gmail.com>
2020-05-07 21:15:20 +0530

    changed naming, added gui file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from Crypto.Random import get_random_bytes
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+import Crypto
 
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 
 filename = askopenfilename() 
 
+t = open(filename,'rb')
+k = t.readlines()
 
-
+print(k)
