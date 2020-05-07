eb02727f9da42c707731918b85c7b0a62374abc9
mathster101 <mathewsimonpanicker@gmail.com>
2020-05-07 21:03:55 +0530

    comm 2

--- proj.py
+++ proj.py
@@ -1,6 +1,11 @@
+from Crypto.Random import get_random_bytes
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
+
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
+
+filename = askopenfilename() 
+
+
+
