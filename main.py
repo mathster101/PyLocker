27ba03006fa3048393a9af6cd481b932efd5bed7
mathster101 <mathewsimonpanicker@gmail.com>
2020-05-11 23:13:14 +0530

    added encryption

--- main.py
+++ main.py
@@ -9,8 +9,28 @@ from gui import *
 salt = b')Ik\xb7\x17\xb7)\x97o\t\xfaB\x0c\xbb\xab\xe2\xa1\x08\xcb\xebS\x12\xe5\xe6\x7fC\xc4P/\x14\xad\xa2'
 password = get_password()
 key = PBKDF2(password, salt, dkLen=32) # 256 bits for aes256 using Password-Based Key Derivation Function
-
+buffer_size = 10000
 data = 'the world is a vampire'
+test_file = 'test.jpg'
 cipher = AES.new(key, AES.MODE_CFB) # CFB mode
-ciphered_data = cipher.encrypt(bytes(data,'utf-8'))
-print(ciphered_data)
+'''
+in_file = open(test_file,'rb')
+out_file = open(test_file+'.hax','wb')
+out_file.write(cipher.iv)
+buffer = in_file.read(buffer_size)
+while len(buffer) > 0:
+    ciphered_bytes = cipher.encrypt(buffer)
+    out_file.write(ciphered_bytes)
+    buffer = in_file.read(buffer_size)
+'''
+def encrypt(file_name):
+	fin = open(file_name,'rb')
+	fout = open(file_name + '.pylocker','wb')
+	buffer  = fin.read(buffer_size)
+	fout.write(cipher.iv)
+	while(len(buffer)>0):
+		ciph = cipher.encrypt(buffer)
+		fout.write(ciph)
+		buffer = fin.read(buffer_size)
+		
+encrypt(test_file)
