cbd285ea757e9200d1438b522bd54d7128b5c606
mathster101 <mathewsimonpanicker@gmail.com>
2020-05-11 23:32:03 +0530

    put in blank funcs

--- gui.py
+++ gui.py
@@ -1,6 +1,16 @@
 # insert all gui stuff here
+def file_picker():
+	return 'file path'
 
+
+def choice():
+#choose wether to encrypt or decrypt""	
+	pass
 
 def get_password():
 	
 	return "test_pass"
+
+def success(task):
+#3ncrypt/decrypt successfull!	
+	pass
